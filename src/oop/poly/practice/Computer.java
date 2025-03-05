5a0e52f9e49db8fa61ad87e2f421d5c91c6bf25c
KangWonBeen <WonKong01@naver.com>
2025-03-06 01:36:53 +0900

    20250304 JAVA  - (연습문제풀이 poly)

---
+++ src/oop/poly/practice/Computer.java
@@ -0,0 +1,25 @@
+package oop.poly.practice;
+
+public class Computer {
+
+    private ComputerPart[] parts;
+
+    public Computer() {
+        this.parts = new ComputerPart[0];
+    }
+
+    public void addPart(ComputerPart part) {
+        ComputerPart[] temp = new ComputerPart[parts.length + 1];
+        for (int i = 0; i < parts.length; i++) {
+            temp[i] = parts[i];
+        }
+        temp[temp.length-1] = part;
+        parts = temp;
+    }
+
+    public void showParts() {
+        for (ComputerPart part : parts) {
+            part.describePart();
+        }
+    }
+}
